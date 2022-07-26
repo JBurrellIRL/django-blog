c518a333de5abdfa7c509d83c3b2fd95115978a7
JBurrellIRL <jburrellirl@gmail.com>
2022-07-26 15:16:57 +0000

    Creating first view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField 
 
-# Create your models here.
+# draft or published posts
+STATUS = ((0, "Draft"), (1, "Published"))
+
+# POST MODEL 
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    # cascade = if we delete our user we also delete our blog posts. One to many relationship
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name = 'blog_likes', blank=True)
+    
+    class Meta:
+        # minus sign means descending order
+        ordering = ['-created_on']
+
+    # below returns a string representation of any object. Good practice 
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+
